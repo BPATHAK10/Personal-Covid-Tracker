882dc9ab1921ea7f98b190a42bad18c07608fae5
Biraj <birajpathak10@gmail.com>
2021-08-22 21:27:07 +0545

    added server and routes to the index.js making these files redundant

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import app from "./server.js"
+
+import UsersController from "./api/user.controller.js"
+import ContactsController from "./api/contacts.controller.js"
+import RelationsController from "./api/relations.controller.js"
+
+// const express = require('express')
 import express from "express"
-import mongodb from "mongodb"
 import dotenv from "dotenv"
+
+import cors from "cors"
+import c_tracker from "./api/c_tracker.route.js"    
+
+import mongodb from "mongodb"
 import UserDAO from "./dao/userDAO.js"
 import ContactsDAO from "./dao/contactsDAO.js"
 import RelationDAO from "./dao/relationsDAO.js"
@@ -13,6 +22,7 @@ const MongoClient = mongodb.MongoClient
 const port = process.env.PORT || 5000   //get the port value from dotenv
 console.log(process.env.NODE_ENV)
 
+const app = express()
 
 //connect to the db
 MongoClient.connect(
@@ -33,15 +43,45 @@ MongoClient.connect(
     await ContactsDAO.injectDB(client)
     await RelationDAO.injectDB(client)
 
-    // if(process.env.NODE_ENV == "production"){
-    //     app.use(express.static("client/build"));
-    //     // const path = require("path")
-    //     app.get("*",(req,res)=>{
-    //         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    //     })
-    // }
+    app.use(cors())
+    app.use(express.json())    // allows to accept json in the body of a request
+    app.use(express.urlencoded())
+    
+    // console.log(process.env.NODE_ENV)
+    app.route("/sign-in").post(UsersController.apiSignIn)
+    app.route("/sign-up").post(UsersController.apiSignUp)
+    app.route("/user/edit").put(UsersController.apiUpdateUser)
+    app.route("/user/contacts/all/:id").get(ContactsController.apiGetContacts)
+    app.route("/contact/add").post(ContactsController.apiPostContact)
+    app.route("/contact/edit").put(ContactsController.apiUpdateContact)
+    app.route("/contact/delete/:id").delete(ContactsController.apiDeleteContact)
+    app.route("/relations/all").get(RelationsController.apiGetRelations)
+    app.route("/relations/add").post(RelationsController.apiPostRelation)
 
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send(`Red Alert ${err.stack}`);
+    });
+    
+    if(process.env.NODE_ENV == "production"){
+        app.use(express.static(path.join("client/build")));
+        // const path = require("path")
+        app.get("*",(req,res)=>{
+            res.sendFile(path.resolve(path.resolve(),'client','build','index.html'))
+        })
+    }
+    // app.use("/api/v1/c_tracker", c_tracker)
+    // app.use("*", (req, res)=> res.status(404).json({error:"not found what???????"}))
+    
+    
     app.listen(port,()=>{
         console.log(`listening on port ${port}`)})
-    }
-)
+})
+
+
+// router.route("/").get(UsersController.apiGetUsers) //creating a route
+
+// export default router
+
+
+// export default app
