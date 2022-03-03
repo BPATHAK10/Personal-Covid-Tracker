c2acce175ca48d0842b889fbd02ff56fc9f74da3
Biraj <birajpathak10@gmail.com>
2022-03-03 22:33:54 +0545

    removed _dirname

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ if(process.env.NODE_ENV === "production"){
         console.log("inside")
         app.use(express.static(path.join("client/build")));
         app.get("*",(req,res)=>{
-            res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+            res.sendFile(path.resolve(path.resolve(), 'client','build','index.html'));
         })
     }
 
