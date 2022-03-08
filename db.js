1b6d3d4e76d695f57dd8b5c8ffc4d95f79e11fb2
Biraj <birajpathak10@gmail.com>
2022-03-08 12:57:20 +0545

    configured ssl to rejectUnauthorized to false

--- db.js
+++ db.js
@@ -5,7 +5,9 @@ var pool;
 if(process.env.NODE_ENV === "production") {
     pool = new Pool({
         connectionString: process.env.DATABASE_URL,
-        ssl: true 
+        ssl: {
+            rejectUnauthorized: false
+        }
     })
 }
 else{
